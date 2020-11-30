20ab84280005dbbc0afb5268ca40ab6e763f0dfe
Olaleye Blessing <52708753+Olaleye-Blessing@users.noreply.github.com>
2020-11-30 23:36:40 +0100

    On branch social_media_dash_accessibility 	modified:   frontendMentor/social-media-dashboard-with-theme-switcher-master/css/main.css -- style the label
    	modified:   frontendMentor/social-media-dashboard-with-theme-switcher-master/index.html -- make the label(mode switch) accessible
    
    	modified:   frontendMentor/social-media-dashboard-with-theme-switcher-master/index.js -- modified the functionality

--- frontendMentor/social-media-dashboard-with-theme-switcher-master/index.js
+++ frontendMentor/social-media-dashboard-with-theme-switcher-master/index.js
@@ -1,31 +1,44 @@
 const body = document.body;
 const toggle = document.querySelector(".mode__toggle");
 const spanToggle = document.querySelector(".mode__toggle-slider");
+const wordToggle = document.querySelector(".mode__toggle-word");
+let cards = document.querySelectorAll('.card');
 
 // I used this because click event is firing twice on label element
 spanToggle.addEventListener("click", (event) => {
     event.stopPropagation();
 });
 
+// I used this so that toggle won't switch when 'Dark-mode word is clicked'
+wordToggle.addEventListener('click', event => {
+    event.preventDefault();
+    event.stopPropagation();
+})
+
 function mode(event) {
     if (body.classList.contains("black-mode")) {
-        body.classList.remove("black-mode");
-        body.classList.add("white-mode");
+        darkMode();
     } else {
-        body.classList.remove("white-mode");
-        body.classList.add("black-mode");
+        lightMode();
     }
-
-    let cards = document.querySelectorAll('.card');
-    cards.forEach(card => {
-        if (card.classList.contains('dark-card')) {
-            card.classList.remove('dark-card');
-            card.classList.add('white-card');
-        } else {
-            card.classList.remove('white-card');
-            card.classList.add('dark-card');
-        }
-    })
 }
 
 toggle.addEventListener("click", mode);
+
+function darkMode() {
+    body.classList.remove("black-mode");
+    body.classList.add("white-mode");
+    cards.forEach(card => {
+        card.classList.remove('dark-card');
+        card.classList.add('white-card');
+    })
+}
+
+function lightMode() {
+    body.classList.remove("white-mode");
+    body.classList.add("black-mode");
+    cards.forEach(card => {
+        card.classList.remove('white-card');
+        card.classList.add('dark-card');
+    })
+}
