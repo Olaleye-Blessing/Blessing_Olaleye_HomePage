65427ca83e595ba54be4ae7934fe85281acb4656
Olaleye Blessing <52708753+Olaleye-Blessing@users.noreply.github.com>
2020-12-21 10:55:17 +0100

    - Restarted frontendMentor/insure-landing-page-master

--- frontendMentor/insure-landing-page-master/index.js
+++ frontendMentor/insure-landing-page-master/index.js
@@ -1,20 +1,20 @@
-const navToggle = document.querySelector(".nav__toggle");
+const btnToggle = document.querySelector('.nav__toggle');
 
-const navLink = document.querySelector('[data-nav="nav-links"]');
+const navLinks = document.querySelector('.nav__links');
 
-navToggle.addEventListener("click", (event) => {
-    let parentLink = document.querySelector("nav ul");
-    if (parentLink.classList.contains("nav__links")) {
-        navToggle.style.backgroundImage = "url(images/icon-hamburger.svg)";
-        parentLink.classList.add("navCloseAni");
-        document.body.style.overflow = '';
+
+btnToggle.addEventListener('click', event => {
+    if (navLinks.classList.contains('hidden')) {
+        navLinks.classList.add('close');
+        btnToggle.style.backgroundImage = 'url(images/icon-hamburger.svg)';
         setTimeout(() => {
-            parentLink.classList.remove("navCloseAni");
-            parentLink.classList.remove("nav__links");
+            navLinks.classList.remove('close');
+            navLinks.classList.remove('hidden');
         }, 1100);
+        document.body.style.overflow = '';
     } else {
-        parentLink.classList.add("nav__links");
-        navToggle.style.backgroundImage = "url(images/icon-close.svg)";
+        navLinks.classList.add('hidden');
+        btnToggle.style.backgroundImage = 'url(images/icon-close.svg)';
         document.body.style.overflow = 'hidden';
     }
-});
+})
