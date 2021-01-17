804e13f1d5b04ea1ff345bfd8df5e068256b227d
Olaleye Blessing <52708753+Olaleye-Blessing@users.noreply.github.com>
2021-01-17 17:06:47 +0100

    Fixed local storage(checkbox for switching mode) for frontendMentor/social-media-dashboard-with-theme-switcher-master/

--- frontendMentor/social-media-dashboard-with-theme-switcher-master/theme.js
+++ frontendMentor/social-media-dashboard-with-theme-switcher-master/theme.js
@@ -3,8 +3,12 @@ const root = document.querySelector('html');
 if (localStorage.getItem('theme')) {
     if (localStorage.getItem('theme') == 'white') {
         root.classList.add('white');
-        setTimeout(() => {
-            toggleBtn.checked = JSON.parse(localStorage.getItem('check'))
-        }, 0);
     }
 }
+
+window.addEventListener('load', () => {
+    toggleBtn.checked = JSON.parse(localStorage.getItem('check'));
+})
+
+// localStorage.clear();
+
